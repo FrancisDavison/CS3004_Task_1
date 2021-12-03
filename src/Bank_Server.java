2eadd386cd64fcc4f6ed0be1a2c65efa88b254af
FrancisDavison <francis.nj.davison@gmail.com>
2021-12-03 18:43:27 +0000

    added comments

--- src/Bank_Server.java
+++ src/Bank_Server.java
@@ -8,39 +8,39 @@ public class Bank_Server
 {
 	public static void main(String args[]) throws IOException
 	{
-		ServerSocket BankServerSocket=null;
-		boolean listening=true;
-		String BankServerName="Bank Server";
-		int BankServerNumber=4545;
+		ServerSocket BankServerSocket=null; //Sets BankServerSocket variable to null, so it can be assigned later in the code
+		boolean listening=true; //Sets listening to true, so it starts the code already listening for connections on port 4545
+		String BankServerName="Bank Server"; //Sets the name for the Bank Sever
+		int BankServerNumber=4545; //Sets the socket number for the server
 		
 		double Balances[]={1000,1000,1000}; //Original double changed to array of doubles to allow three values to be stored in one variable
 		
 		//Create the shared object in the global scope...
 		
-		Shared_Bank_State OurSharedBankStateObject = new Shared_Bank_State(Balances);
+		Shared_Bank_State OurSharedBankStateObject = new Shared_Bank_State(Balances); //Sends the initial state of the shared variable double balaces[] line (above)
 		
 		//Make the server socket
 		
 		try
 		{
-			BankServerSocket = new ServerSocket(BankServerNumber);
+			BankServerSocket = new ServerSocket(BankServerNumber); //Attempts to start the server socket
 		}
-		catch(IOException e)
+		catch(IOException e) //If server socket could not be started, IOException will be thrown and dealt with here
 		{
-			System.err.println("Could not start "+BankServerName+" specified port.");
+			System.err.println("Could not start "+BankServerName+" specified port."); //Inform user that server could not be started on selected port
 			System.exit(1);
 		}
-		System.out.println(BankServerName+" started");
+		System.out.println(BankServerName+" started"); //Inform user that server started successfully
 		
-		while(listening)
+		while(listening) //essentially while(true), so will always be listeining on port 4545
 		{
-			new Bank_Server_Thread(BankServerSocket.accept(), "BankServerThread1", OurSharedBankStateObject).start();
-			System.out.println("New " + BankServerName + " thread started.");
-			new Bank_Server_Thread(BankServerSocket.accept(), "BankServerThread2", OurSharedBankStateObject).start();
-			System.out.println("New " + BankServerName + " thread started.");
-			new Bank_Server_Thread(BankServerSocket.accept(), "BankServerThread3", OurSharedBankStateObject).start();
-			System.out.println("New " + BankServerName + " thread started.");
+			new Bank_Server_Thread(BankServerSocket.accept(), "BankServerThread1", OurSharedBankStateObject).start(); //When thread 1 is requested, this line accepts it
+			System.out.println("New " + BankServerName + " thread started."); //Inform the user that the thread has been started
+			new Bank_Server_Thread(BankServerSocket.accept(), "BankServerThread2", OurSharedBankStateObject).start(); //When thread 2 is requested, this line accepts it
+			System.out.println("New " + BankServerName + " thread started."); //Inform the user that the thread has been started
+			new Bank_Server_Thread(BankServerSocket.accept(), "BankServerThread3", OurSharedBankStateObject).start(); //When thread 3 is requested, this line accepts it
+			System.out.println("New " + BankServerName + " thread started."); //Informs the user that the thread has been started
 		}
-		BankServerSocket.close();
+		BankServerSocket.close(); //Closes socket
 	}
 }
