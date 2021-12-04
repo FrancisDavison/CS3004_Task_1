efcc2574fc7bc5cb45f475510bd8f80c79f2acdf
FrancisDavison <francis.nj.davison@gmail.com>
2021-12-04 14:31:49 +0000

    added comments

--- src/Bank_Server_Thread.java
+++ src/Bank_Server_Thread.java
@@ -5,51 +5,50 @@ import java.net.*;
 
 public class Bank_Server_Thread extends Thread
 {
-	private Socket BankSocket=null;
-	private Shared_Bank_State MySharedBankStateObject;
-	private String MyBankServerThreadName;
-	private double MySharedVariable;
+	private Socket BankSocket=null; //Setting socket name for BankSocket to null
+	private Shared_Bank_State MySharedBankStateObject; //Setting Shared_bank_State object equal to MySharedBankStateObject
+	private String MyBankServerThreadName; //Initialising string for MyBankServerThreadName
+	private double MySharedVariable; //Initialising double MySharedVariable
 	
 	//Set up the thread
 	public Bank_Server_Thread(Socket BankSocket, String BankServerThreadName, Shared_Bank_State SharedObject)
 	{
-		this.BankSocket=BankSocket;
-		MySharedBankStateObject=SharedObject;
-		MyBankServerThreadName=BankServerThreadName;
+		this.BankSocket=BankSocket; //THIS lol
+		MySharedBankStateObject=SharedObject; //Setting MySharedBankStateObject equal to SharedObject
+		MyBankServerThreadName=BankServerThreadName; //Setting MyBankServerThreadName equal to BankServerThreadName
 	}
 	
-	public void run()
+	public void run() //This run allows threads to request a lock, acquire a lock, or release a lock when needed
 	{
 		try
 		{
-			System.out.println(MyBankServerThreadName+" initialising");
-			PrintWriter out=new PrintWriter(BankSocket.getOutputStream(), true);
-			BufferedReader in=new BufferedReader(new InputStreamReader(BankSocket.getInputStream()));
-			String InputLine, OutputLine;
+			System.out.println(MyBankServerThreadName+" initialising"); //Inform user thread is being initialised
+			PrintWriter out=new PrintWriter(BankSocket.getOutputStream(), true); //PrintWriter stuff, can't remember
+			BufferedReader in=new BufferedReader(new InputStreamReader(BankSocket.getInputStream())); //BufferedReader from socket input
+			String InputLine, OutputLine; //Initialising InputLine and OutputLine Strings to be used later
 			
-			while((InputLine=in.readLine())!=null)
+			while((InputLine=in.readLine())!=null) //While there is a user input to read...
 			{
-				//Get a lock first
-				try
+				try //Get a lock, then call ProcessInput to perform the analysis and carry out the action
 				{
-					MySharedBankStateObject.AcquireLock();
-					OutputLine=MySharedBankStateObject.ProcessInput(MyBankServerThreadName, InputLine);
-					out.println(OutputLine);
-					MySharedBankStateObject.ReleaseLock();
+					MySharedBankStateObject.AcquireLock(); //Acquires lock
+					OutputLine=MySharedBankStateObject.ProcessInput(MyBankServerThreadName, InputLine); // Calls ProcessInput and passes Client ThreadName, and then user input, this returns OutputLine string
+					out.println(OutputLine); //Prints OutputLine string
+					MySharedBankStateObject.ReleaseLock(); //Releases lock and allows another thread to perform action.
 				}
-				catch(InterruptedException e)
+				catch(InterruptedException e) //If lock failed to be established, InterruptedException is thrown and dealt with here
 				{
-					System.err.println("Failed to get lock when reading: "+e);
+					System.err.println("Failed to get lock when reading: "+e); //Inform user that lock failed to be established
 				}
 			}
-			out.close();
-			in.close();
-			BankSocket.close();
+			out.close(); //Close PrintWriter out
+			in.close(); //Close BufferedReader in
+			BankSocket.close(); //Close sokcet
 		}
 		
-		catch(IOException e)
+		catch(IOException e) //Catch IOException
 		{
-			e.printStackTrace();
+			e.printStackTrace(); //Print error
 		}
 	}
 }
